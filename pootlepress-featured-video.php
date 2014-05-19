e921fe8a64dde1c4b0c6bafe28244ee5a3dc64d7
Nick Burne <nickburne@users.noreply.github.com>
2014-05-19 08:40:48 +0100

    Update pootlepress-featured-video.php

--- pootlepress-featured-video.php
+++ pootlepress-featured-video.php
@@ -1,9 +1,9 @@
 <?php
 /*
-Plugin Name: Canvas Extension - Featured Video
+Plugin Name: Canvas Extension - Featured Video for Business Slider
 Plugin URI: http://pootlepress.com/
 Description: An extension for WooThemes Canvas that allow you to use featured video in slider
-Version: 1.0.0
+Version: 1.1.0
 Author: PootlePress
 Author URI: http://pootlepress.com/
 License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
@@ -16,6 +16,6 @@ require_once( 'pootlepress-featured-video-functions.php' );
 require_once( 'classes/class-pootlepress-featured-video.php' );
 
 $GLOBALS['pootlepress_featured_video'] = new Pootlepress_Featured_Video( __FILE__ );
-$GLOBALS['pootlepress_featured_video']->version = '1.0.0';
+$GLOBALS['pootlepress_featured_video']->version = '1.1.0';
 
 ?>
